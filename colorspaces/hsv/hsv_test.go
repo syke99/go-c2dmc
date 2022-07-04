be7c8af1794457ff1fbac58ddab728b041727579
syke99 <quinnmillican@gmail.com>
2022-07-04 10:29:10 -0500

    beginning of test refactor

--- colorspaces/hsv/hsv_test.go
+++ colorspaces/hsv/hsv_test.go
@@ -1,30 +1,84 @@
 package hsv
 
 import (
-	"github.com/syke99/go-c2dmc/colorBank"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHsvRgb(t *testing.T) {
-	cb := colorBank.New()
-	r, g, b := cb.Hsv.HsvToRgb(0.000000, 0.2938775510204082, 245.000000)
-	assert.Equal(t, 245.0, r)
-	assert.Equal(t, 173.0, g)
-	assert.Equal(t, 173.0, b)
+//-----------
+// mocks
+//-----------
+
+type mockColorBank struct {
+	fail bool
 }
 
-func TestHsvHex(t *testing.T) {
-	cb := colorBank.New()
-	hex := cb.Hsv.HsvToHex(0.000000, 0.2938775510204082, 245.000000)
+func (m mockColorBank) MockHsvToRgb() ([]float64, error) {
+	if m.fail {
+		return []float64{}, errors.New("failure")
+	}
+	return []float64{245.0, 173.0, 173.0}, nil
+}
+
+func (m mockColorBank) MockHsvToHex() (string, error) {
+	if m.fail {
+		return "", errors.New("failure")
+	}
+	return "#0b5353", nil
+}
+
+func (m mockColorBank) MockHsvToLab() ([]float64, error) {
+	if m.fail {
+		return []float64{}, errors.New("failure")
+	}
+	return []float64{74.28679665732454, 22.545819185702953, 9.144645443127331}, nil
+}
+
+//-----------
+// test mocks
+//-----------
+
+func TestHsvRgb_NoFail(t *testing.T) {
+	cb := mockColorBank{}
+	rgb, err := cb.MockHsvToRgb()
+	assert.Equal(t, 245.0, rgb[0])
+	assert.Equal(t, 173.0, rgb[1])
+	assert.Equal(t, 173.0, rgb[2])
+	assert.NoError(t, err)
+}
+
+func TestHsvRgb_Fail(t *testing.T) {
+	cb := mockColorBank{fail: true}
+	_, err := cb.MockHsvToRgb()
+	assert.Error(t, err)
+}
+
+func TestHsvHex_NoFail(t *testing.T) {
+	cb := mockColorBank{}
+	hex, err := cb.MockHsvToHex()
 	assert.Equal(t, "#0b5353", hex)
+	assert.NoError(t, err)
 }
 
-func TestHsvLab(t *testing.T) {
-	cb := colorBank.New()
-	l, a, b := cb.Hsv.HsvToLab(0.000000, 0.2938775510204082, 245.000000)
-	assert.Equal(t, 74.28679665732454, l)
-	assert.Equal(t, 22.545819185702953, a)
-	assert.Equal(t, 9.144645443127331, b)
+func TestHsvHex_Fail(t *testing.T) {
+	cb := mockColorBank{fail: true}
+	_, err := cb.MockHsvToHex()
+	assert.Error(t, err)
+}
+
+func TestHsvLab_NoFail(t *testing.T) {
+	cb := mockColorBank{}
+	lab, err := cb.MockHsvToLab()
+	assert.Equal(t, 74.28679665732454, lab[0])
+	assert.Equal(t, 22.545819185702953, lab[1])
+	assert.Equal(t, 9.144645443127331, lab[2])
+	assert.NoError(t, err)
+}
+
+func TestHsvLab_Fail(t *testing.T) {
+	cb := mockColorBank{fail: true}
+	_, err := cb.MockHsvToLab()
+	assert.Error(t, err)
 }
